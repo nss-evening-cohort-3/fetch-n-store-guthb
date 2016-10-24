b6c2a32e9f9f14c3e04e65aade6a29275e58a574
Bradley Guthrie <guppy96@gmail.com>
2016-10-24 16:11:54 -0500

    status and times return with cors enabled

--- FetchAndStore/FetchAndStore/App/Controllers/SearchUrlCtrl.js
+++ FetchAndStore/FetchAndStore/App/Controllers/SearchUrlCtrl.js
@@ -3,17 +3,18 @@
     //set the varibles to empty
     $scope.url = "";
     $scope.HttpMethod = "";
+    $scope.ResponseTime = "";
 
     //Function called from action from DOM
-    $scope.HttpFetcher = function (    ) {
+    $scope.HttpFetcher = function ( methodPicked, urlEntered ) {
 
         //Duration of respose
-
-
+        var startTime = Date.now();
 
         //call to url based on http method requested 
-        $http({ method: $scope.HttpMethod, url: $scope.url, cache: $templateCache }).
+        $http({ method: $scope.HttpMethod, url: $scope.url }).
         then(function (response) {
+            $scope.ResponseTime = Date.now() - startTime;
             $scope.status = response.status;
             $scope.data = response.data;
         }, function (response) {
